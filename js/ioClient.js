894e352a0bc9d89ac851ffceb4a1de94b95dbac3
dr0n <kornienko.andriy@gmail.com>
2021-05-24 17:18:34 +0300

    remove vertical template (one template for all orientation)

--- js/ioClient.js
+++ js/ioClient.js
@@ -33,13 +33,13 @@ function openWSConnection(protocol, hostname, port, endpoint) {
 
     webSocket.on('new parameters', (parameters) => {
       console.dir(parameters, { depth: null, colors: true });
-      let template = document.getElementById('horizontalTemplate').innerHTML;
-      if (parameters.orientation !== 0) {
-        template = document.getElementById('verticalTemplate').innerHTML;
-      }
-      else {
-          template = document.getElementById('horizontalTemplate').innerHTML;
-      }
+      let template = document.getElementById('template').innerHTML;
+      // if (parameters.orientation !== 0) {
+      //   template = document.getElementById('template').innerHTML;
+      // }
+      // else {
+      //     template = document.getElementById('template').innerHTML;
+      // }
       renderTemplate(parameters, template);
     });
 
